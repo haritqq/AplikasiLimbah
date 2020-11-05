5c4462a1c422035125be195ff0a8f2eb54a93c56
haritqq <muhammad.harit@gmail.com>
2020-11-05 08:30:23 +0700

    git add .

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -50,4 +50,26 @@ class Mahasiswa extends CI_Controller{
         $this->load->view('edit', $data);
         $this->load->view('templates/footer');
     }
+    public function update(){
+        $id = $this->input->post('id');
+        $nama = $this->input->post('nama');
+        $nim = $this->input->post('nim');
+        $tgl_lahir = $this->input->post('tgl_lahir');
+        $jurusan = $this->input->post('jurusan');
+
+        $data = array(
+            'nama'      => $nama,
+            'nim'       => $nim,
+            'tgl_lahir' => $tgl_lahir,
+            'jurusan'   => $jurusan
+        );
+
+        $where = array(
+            'id'    => $id
+        );
+
+        $this->m_mahasiswa->update_data($where,$data,'tb_mahasiswa');
+        redirect('mahasiswa/index/');
+    }
+
 }  
--- application/models/m_mahasiswa.php
+++ application/models/m_mahasiswa.php
@@ -18,32 +18,9 @@ class M_mahasiswa extends CI_Model{
     public function edit_data($where,$table){
         return $this->db->get_where($table,$where);
     }
-
-    public function update(){
-        $id = $this->input->post('id');
-        $nama = $this->input->post('nama');
-        $nim = $this->input->post('nim');
-        $tgl_lahir = $this->input->post('tgl_lahir');
-        $jurusan = $this->input->post('jurusan');
-
-        $data = array(
-            'nama'      => $nama,
-            'nim'       => $nim,
-            'tgl_lahir' => $tgl_lahir,
-            'jurusan'   => $jurusan
-        );
-
-        $where = array(
-            'id'    => $id
-        );
-
-        $this->m_mahasiswa->update_data($where,$data,'tb_mahasiswa');
-        redirect('mahasiswa/index');
-    }
-
+    
     public function update_data($where,$data,$table){
         $this->db->where($where);
         $this->db->update($table,$data);
     }
-    
 }
